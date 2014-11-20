5fc4e34908505088b04efc4ddbe1b4affbb09e9d
Jeff Nickoloff <jeff@allingeek.com>
2014-11-19 16:53:34 -0800

    Updated to run with an example user.

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         SomeResource res = new SomeResource();
         try {
-            System.out.println("This is my other app. :)"
+            System.out.println("This is my other app. :)");
         } finally {
             res.close();
         }
